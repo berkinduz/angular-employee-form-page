ea8e427c9f82d22acb6882a09fc456ad0f9c6e06
Berkin Düz <64802864+berkinduz@users.noreply.github.com>
2022-02-07 10:16:33 +0300

    login component styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component'
+import { LoginComponent } from './login/login.component'
+import { SignupComponent } from './signup/signup.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: EmployeeDashboardComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
